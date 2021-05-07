d5cbd1f66d4153b3ef6daf418574e86a1a9e7cd2
arjit-choudhary <74242843+arjit-choudhary@users.noreply.github.com>
2021-05-07 16:33:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Text, View, Button,TouchableOpacity } from 'react-native';
+import {Audio} from "expo-av";
+
+class SoundButton extends React.Component {
+   playSound = async () => {
+     await Audio.Sound.createAsync(
+       {uri:"https://soundbible.com/mp3/Buzzer-SoundBible.com-188422102.mp3"},
+       {shouldPlay:true}
+     )
+  }
+
+  render() {
+    return (
+     <TouchableOpacity style={{
+       backgroundColor:"red",
+       marginLeft:108,
+       borderWidth:1,
+       borderColor:"blue",
+       alignItems:"center",
+       justifyContent:"center",
+       width:200,height:200,
+       borderRadius:100
+     }}
+     onPress={this.playSound}>
+     <Text>press me </Text>
+     </TouchableOpacity>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{marginTop:200}}>
+        <SoundButton />
+      </View>
+    );
+  }
+}
+
+
